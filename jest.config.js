ab476e6663448ab6397f5bd142629a25cc5e3b90
Massimo Parzanese <massimoparzanese@gmail.com>
2025-09-11 16:14:23 -0300

    Add new config from tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,27 @@
+/*
 module.exports = {
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
+};
+*/
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom', // jsdom para React, sirve también para lógica backend
+  testMatch: ['**/tests/**/*.test.ts', '**/tests/**/*.test.tsx'], // todos tus tests
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest', // TS y TSX
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom'], // para matcher de RTL
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1', // para resolver imports con @/
+  },
+  globals: {
+    'ts-jest': {
+      isolatedModules: true, // más rápido para tests pequeños
+    },
+  },
 };
